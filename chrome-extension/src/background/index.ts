c2627bed3a3957a54962254d9822f31a945e1f3a
drussov <russov@gmail.com>
2025-04-11 14:13:46 +0300

    Updated TODO

--- chrome-extension/src/background/index.ts
+++ chrome-extension/src/background/index.ts
@@ -7,6 +7,7 @@ import { processXHR } from './parser';
 // TODO: Add clear storage button - check
 // TODO: Display records in info popup
 // TODO: Add csv export and copy to clipboard functionality
+// TODO: Problem with initial value for storage
 
 configStorage.get().then(data => {
   console.log('Startup config', data);
