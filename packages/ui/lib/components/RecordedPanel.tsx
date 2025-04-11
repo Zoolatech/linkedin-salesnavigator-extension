f79c00fa76181c3c69b77fb47bcbd8f0f69fdf4f
drussov <russov@gmail.com>
2025-04-11 19:16:12 +0300

    Removed commented code

--- packages/ui/lib/components/RecordedPanel.tsx
+++ packages/ui/lib/components/RecordedPanel.tsx
@@ -72,37 +72,5 @@ export const RecordedPanel = ({ className, children, ...props }: RecordedPanelPr
       </div>
       {children}
     </div>
-
-    // <div
-    //   className={cn(
-    //     className,
-    //     `flex justify-between gap-4 border rounded-lg p-2`,
-    //     isLight ? 'border-gray-800' : 'border-slate-50 ',
-    //   )}
-    //   {...props}>
-    //   <div className={cn('flex justify-between basis-2/5')}>
-    //     <span>{t('items_status')}</span>
-    //     <span>
-    //       {Object.keys(recording.data.entity).reduce((acc, key) => {
-    //         return acc + (recording.data.entity[key]?.length || 0);
-    //       }, 0)}
-    //     </span>
-    //   </div>
-    //   <div className={cn('flex justify-between basis-2/5')}>
-    //     <span>{t('digging_status')}</span>
-    //     <span>
-    //       {recording.data.fetched.length - recording.toFetchLeft} / {recording.data.fetched.length}
-    //     </span>
-    //   </div>
-    //   <div className={cn('flex justify-between basis-auto')}>
-    //     <button
-    //       onClick={() => recordingStorage.set(recordingInitialState)}
-    //       className={cn('text-red-500')}
-    //       title={t('delete_hint')}>
-    //       {t('delete_button')}
-    //     </button>
-    //   </div>
-    //   {children}
-    // </div>
   );
 };
